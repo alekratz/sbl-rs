de95faeaa8240858bdedca81c6560813b6880f94
Alek Ratzloff <alekratz@gmail.com>
2017-07-28 18:36:56 -0400

    Re org (#26)
    * Move project to library project, with a binary frontend. Also rename the project to sbl.
    
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>
    
    * Update README.md to reflect recent changes.
    
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#[macro_use]
+extern crate error_chain;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate maplit;
+#[macro_use]
+extern crate matches;
+#[macro_use]
+extern crate enum_methods;
+extern crate libc;
+extern crate libffi;
+
+pub mod syntax;
+pub mod compile;
+pub mod vm;
+pub mod common;
+
+pub mod errors {
+    use common::*;
+
+    error_chain! {
+        errors {
+            Ranged(range: Range) {
+                description("Ranged error")
+                display("in {}", range)
+            }
+        }
+    }
+
+    impl From<Range> for ErrorKind {
+        fn from(r: Range) -> ErrorKind {
+            ErrorKind::Ranged(r)
+        }
+    }
+}
