6ab9de43f2341d0bc5953b93187e0e953c4d902b
Alek Ratzloff <alekratz@gmail.com>
2017-08-15 19:29:07 -0400

    Undo rustfmt in bake.rs in a couple places, specifically the iter chaining
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- src/compile/bake.rs
+++ src/compile/bake.rs
@@ -32,16 +32,8 @@ impl Compile for BakeBytes {
             // gets a list of all the bake blocks for this function
             let bake_blocks = fun.body
                 .iter()
-                .filter_map(|b| if b.bc_type == BcType::Bake {
-                    b.val.clone()
-                } else {
-                    None
-                })
-                .filter_map(|v| if let Val::BakeBlock(b) = v {
-                    Some(b)
-                } else {
-                    None
-                })
+                .filter_map(|b| if b.bc_type == BcType::Bake { b.val.clone() } else { None })
+                .filter_map(|v| if let Val::BakeBlock(b) = v { Some(b) } else { None })
                 .collect::<Vec<_>>();
 
             // what the below does is gets the list of function calls in all bake statements that
@@ -49,41 +41,16 @@ impl Compile for BakeBytes {
             let illegal_calls = bake_blocks
                 .iter()
                 .flat_map(|b| &b.block)
-                .filter_map(|stmt| if let &Stmt::Stack(ref stmt) = stmt {
-                    Some(&stmt.stack_actions)
-                } else {
-                    None
-                })
+                .filter_map(|stmt| if let &Stmt::Stack(ref stmt) = stmt { Some(&stmt.stack_actions) } else { None })
                 .flat_map(|actions| actions)
-                .filter_map(|a| if let &StackAction::Push(ref i) = a {
-                    Some(i)
-                } else {
-                    None
-                })
-                .filter_map(|i| if let ItemType::Ident(ref fname) = i.item_type {
-                    self.fun_table.get(fname)
-                } else {
-                    None
-                })
-                .filter_map(|fun| if let &Fun::UserFun(ref fun) = fun {
-                    if fun.contains_bake {
-                        Some(&fun.name)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                })
+                .filter_map(|a| if let &StackAction::Push(ref i) = a { Some(i) } else { None })
+                .filter_map(|i| if let ItemType::Ident(ref fname) = i.item_type { self.fun_table.get(fname) } else { None })
+                .filter_map(|fun| if let &Fun::UserFun(ref fun) = fun { if fun.contains_bake { Some(&fun.name) } else { None } } else { None })
                 .collect::<Vec<_>>();
 
             // Check if there's any recursive bake blocks
             if !illegal_calls.is_empty() {
-                return (Err(
-                    format!(
-                        "attempted to call `{}`, which contains a `bake` statement itself",
-                        illegal_calls[0]
-                    ).into(),
-                ) as Result<_>)
+                return (Err(format!("attempted to call `{}`, which contains a `bake` statement itself", illegal_calls[0]).into()) as Result<_>)
                     .chain_err(|| format!("in `bake` statement in function `{}`", fun.name))
                     .chain_err(|| "recursive `bake` statements are not allowed");
             }
@@ -128,13 +95,10 @@ impl Compile for BakeBytes {
                     match vm.invoke(&baked_name) {
                         Ok(_) => {
                             let vm_state: State = vm.into();
-                            Ok(
-                                vm_state
-                                    .stack
+                            Ok(vm_state.stack
                                     .into_iter()
                                     .map(|val| Bc::push(block_tokens.clone(), val))
-                                    .collect::<Vec<_>>(),
-                            )
+                                    .collect::<Vec<_>>())
                         }
                         Err(e) => Err(e),
                     }
