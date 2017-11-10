5c7fddeea8c729b6a9dc4ca24e35a2eb549fb23b
Alek Ratzloff <alekratz@gmail.com>
2017-11-10 17:24:29 -0500

    Add br-el block tests
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- tests/test-branch.rs
+++ tests/test-branch.rs
@@ -6,7 +6,6 @@ use sbl::prelude::*;
 macro_rules! state_test {
     ($code:expr, $expected:expr) => {{
         let test_code = $code;
-
         let tokenizer = Tokenizer::new("test", &test_code);
         let mut parser = Parser::new(tokenizer);
         let ast = AST {
@@ -15,13 +14,16 @@ macro_rules! state_test {
         }.preprocess::<&str>(&[]).expect("Preprocess error");
         let ir_compiler = CompileIR::new(&ast);
         let bc_compiler = CompileBytes::new(ir_compiler.compile().expect("IR compile error"));
-
         let fun_table = bc_compiler.compile().expect("BC compile error");
         let mut vm = VM::new(fun_table);
         vm.run().expect("Runtime error");
         assert_eq!(vm.state().stack, $expected);
     }}
 }
+// TODO
+// Have this macro also run optimizations, because we want to know immediately when an optimization breaks current implementations
+// TODO
+
 
 #[test]
 fn test_solo_br() {
@@ -37,7 +39,8 @@ fn test_solo_br() {
     //state_test!(r#"main { br "asdf" "asdf" == { 9999 } }"#, vec![BCVal::Int(9999)]);
     state_test!(r#"main { T br { 9999 } }"#, vec![BCVal::Int(9999)]);
     state_test!(r#"main { br F { 9999 } br T { 55555 } }"#, vec![BCVal::Int(55555)]);
-    // Funky nested statements
+    state_test!(r#"main { br 0 { 9999 } }"#, vec![BCVal::Int(9999)]);
+    // Nested statements
     state_test!(r#"main { br T { br T { 55555 } } }"#, vec![BCVal::Int(55555)]);
     state_test!(r#"main { T br { T br { 9999 } br T { 55555 } } }"#, vec![BCVal::Int(9999), BCVal::Int(55555)]);
     state_test!(r#"main { br 9999 55555 { } }"#, vec![BCVal::Int(9999)]);
@@ -45,7 +48,15 @@ fn test_solo_br() {
 
 #[test]
 fn test_br_el() {
-
+    // Rudimentary else tests
+    state_test!(r#"main { br F { 8888 } el { 9999 } }"#, vec![BCVal::Int(9999)]);
+    state_test!(r#"main { br F { 8888 } el { 9999 7777 } }"#, vec![BCVal::Int(9999), BCVal::Int(7777)]);
+    state_test!(r#"main { br T { 8888 } el { 9999 } }"#, vec![BCVal::Int(8888)]);
+    state_test!(r#"main { br T { 8888 } el { 9999 7777 } }"#, vec![BCVal::Int(8888)]);
+    state_test!(r#"main { br T { 8888 } el { 9999 } 7777 }"#, vec![BCVal::Int(8888), BCVal::Int(7777)]);
+    // Nested statements
+    state_test!(r#"main { br F { 8888 } el { br 6666 F { 7777 } el { 9999 } } } "#, vec![BCVal::Int(6666), BCVal::Int(9999)]);
+    state_test!(r#"main { br 6666 F { 8888 } el { br F { 7777 } el { 9999 } } } "#, vec![BCVal::Int(6666), BCVal::Int(9999)]);
 }
 
 #[test]
